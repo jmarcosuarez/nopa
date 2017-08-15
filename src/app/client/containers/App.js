3afcfecd90f4ed247760c02ac5893e2212a12582
Marco Suarez <jmarcosuarez@gmail.com>
2017-08-15 10:48:00 +0100

    Refactor styles

--- src/app/client/containers/App.js
+++ src/app/client/containers/App.js
@@ -1,10 +1,11 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { PropTypes } from 'react';
+import styles from '../components/Layout/Layout.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className={styles.container}>
         {this.props.children}
       </div>
     );
